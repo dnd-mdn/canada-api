3869a74032a7e8578d05589dc6b2a01704ff07cf
Ben Soicher <ben.soicher@gmail.com>
2022-04-01 14:06:08 -0400

    bundle as es5

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  target: ['web', 'es5'],
   output: {
     path: path.resolve(__dirname, './dist/'),
     filename: 'ca.js',
